33601c317837f5bc27c9c31997814c9e378dcc9d
Alexander Kryuk <kryukalexander@gmail.com>
2018-04-16 11:12:36 +0600

    Added html beautification

--- posthtml.config.js
+++ posthtml.config.js
@@ -9,6 +9,7 @@ let locals = {
 module.exports = {
     plugins: [
         require('posthtml-include')({}),
-        require('posthtml-expressions')(locals)
+        require('posthtml-expressions')(locals),
+        require('posthtml-beautify')({ rules: { indent: 4, blankLines: '' }})
     ]
 };
